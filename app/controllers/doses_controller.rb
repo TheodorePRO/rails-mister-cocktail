42034c90e4d83b689b2f16ec57d80cfdc13640b3
Theodore JUDLIN <theodore.judlin@free.fr>
2019-11-15 11:19:59 +0100

    Initial commit

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,20 @@
+class DosesController < ApplicationController
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    @ingredient = Ingredient.find(params[:dose][:ingredient])
+    @dose.ingredient = @ingredient
+
+    if @dose.save
+      redirect_to cocktail_path(@dose.id)
+    else
+      render :@cocktail
+    end
+  end
+
+  private
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+ingredient_list = open(url).read
+ingredients = JSON.parse(ingredient_list)
+
+ingredients['drinks'].each do |i|
+
+  Ingredient.create(name: i['strIngredient1'])
+end
